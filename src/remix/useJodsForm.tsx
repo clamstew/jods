140d5289dede3cdde8afb4b0efa348b028a09eac
Clay Stewart <claytonmstewart@gmail.com>
2025-05-03 16:57:53 -0500

    Refactor useJodsForm to dynamically import React hooks, enhancing compatibility and preventing bundling issues. Update HiddenInput and FormWrapper to utilize createElement for improved flexibility and prop handling.

--- src/remix/useJodsForm.tsx
+++ src/remix/useJodsForm.tsx
@@ -1,19 +1,38 @@
 /** @jsxImportSource react */
 import { useFetcher } from "@remix-run/react";
 
+// Dynamic import for React to prevent bundling issues
+function getReactHooks() {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const React = require("react");
+    return {
+      createElement: React.createElement,
+    };
+  } catch (e) {
+    throw new Error("React is required but could not be loaded");
+  }
+}
+
 export function useJodsForm(store: any, handler: string) {
   const fetcher = useFetcher();
+  const { createElement } = getReactHooks();
 
-  const HiddenInput = (
-    <input type="hidden" name="_jods_handler" value={handler} />
-  );
+  const HiddenInput = createElement("input", {
+    type: "hidden",
+    name: "_jods_handler",
+    value: handler,
+  });
 
-  const FormWrapper = (props: any) => (
-    <fetcher.Form method="post">
-      {HiddenInput}
-      {props.children}
-    </fetcher.Form>
-  );
+  const FormWrapper = (props: any) => {
+    // Extract children and spread all other props to the form
+    const { children, ...formProps } = props;
+
+    return createElement(fetcher.Form, { method: "post", ...formProps }, [
+      HiddenInput,
+      children,
+    ]);
+  };
 
   return {
     fetcher,
