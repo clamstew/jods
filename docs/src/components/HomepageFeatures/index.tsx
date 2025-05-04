eaf1d0565493806ef85e425ad452bc3525dabf31
Clay Stewart <claytonmstewart@gmail.com>
2025-05-03 19:10:57 -0500

    Remove deprecated HomepageFeatures component and associated styles to streamline the codebase. This component is no longer in use as it has been replaced with new implementations.

--- docs/src/components/HomepageFeatures/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-// This is a placeholder to avoid breaking import references
-// We're not using this component anymore as it's been replaced with the new components
-export default function HomepageFeatures(): React.ReactElement {
-  return <></>;
-}
